7f432f808fde61b0a97c113ef6962c9410380067
Felipe Leal <felipewanderson2020@gmail.com>
2020-06-30 18:10:45 -0300

    page of register sale

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,13 +1,137 @@
 import React from 'react';
 
 import Header from '../../Components/Header';
-import { Container } from './styles';
+import { Container, Field, FieldGroup, Button } from './styles';
 
 const Register: React.FC = () => {
   return (
     <>
       <Header size="small" />
-      <Container />
+      <Container>
+        <h1>Cadastrar venda</h1>
+        <form>
+          <fieldset>
+            <legend>
+              <h2>Venda</h2>
+            </legend>
+          </fieldset>
+
+          <Field>
+            <label htmlFor="property">Empreedimento</label>
+            <input id="property" name="property" type="text" />
+          </Field>
+          <FieldGroup>
+            <Field>
+              <label htmlFor="unity">Unidade</label>
+              <input id="unity" name="unity" type="text" />
+            </Field>
+            <Field>
+              <label htmlFor="dateSale">Data da venda</label>
+              <input
+                id="dateSale"
+                name="dateSale"
+                type="date"
+                placeholder="DD/MM/AAAA"
+              />
+            </Field>
+          </FieldGroup>
+          <Field>
+            <label htmlFor="valueProperty">Valor do Imovel</label>
+            <input id="valueProperty" name="valueProperty" type="number" />
+          </Field>
+
+          <FieldGroup>
+            <Field>
+              <label htmlFor="realtorSale">Corretor Vendedor</label>
+              <input id="realtorSale" name="realtorSale" type="text" />
+            </Field>
+            <Field>
+              <label htmlFor="realtorCap">Corretor Captador</label>
+              <input id="realtorCap" name="realtorCap" type="text" />
+            </Field>
+          </FieldGroup>
+
+          <FieldGroup>
+            <Field>
+              <label htmlFor="coodSale">Coordenador da Venda</label>
+              <input id="coodSale" name="coodSale" type="text" />
+            </Field>
+            <Field>
+              <label htmlFor="DirSale">Gerente ou Diretor(a) da venda</label>
+              <input id="DirSale" name="DirSale" type="text" />
+            </Field>
+          </FieldGroup>
+
+          <fieldset>
+            <legend>
+              <h2>Comprador</h2>
+            </legend>
+          </fieldset>
+
+          <Field>
+            <label htmlFor="name">Nome Completo</label>
+            <input id="name" name="name" type="text" />
+          </Field>
+
+          <FieldGroup>
+            <Field>
+              <label htmlFor="cpf">CPF</label>
+              <input id="cpf" name="cpf" type="text" />
+            </Field>
+            <Field>
+              <label htmlFor="tel">Telefone</label>
+              <input id="tel" name="tel" type="tel" />
+            </Field>
+          </FieldGroup>
+
+          <Field>
+            <label htmlFor="email">E-mail</label>
+            <input id="email" name="email" type="email" />
+          </Field>
+
+          <fieldset>
+            <legend>
+              <h2>Vendedor</h2>
+            </legend>
+          </fieldset>
+
+          <Field>
+            <label htmlFor="builder">Construtora / Cliente Vendedor </label>
+            <input id="builder" name="builder" type="type" />
+          </Field>
+
+          <fieldset>
+            <legend>
+              <h2>Comissionamento</h2>
+            </legend>
+          </fieldset>
+
+          <FieldGroup>
+            <Field>
+              <label htmlFor="valueCommision">Valor do Comissionamento</label>
+              <input id="valueCommision" name="valueCommision" type="number" />
+            </Field>
+            <Field>
+              <label htmlFor="porcent">Porcentagem</label>
+              <input id="porcent" name="porcent" type="number" />
+            </Field>
+          </FieldGroup>
+
+          <fieldset>
+            <legend>
+              <h2>Observação da Negociação</h2>
+            </legend>
+          </fieldset>
+
+          <Field>
+            <textarea rows={4} cols={50}>
+              informe as observações aqui
+            </textarea>
+          </Field>
+
+          <Button type="submit">Cadastrar venda</Button>
+        </form>
+      </Container>
     </>
   );
 };
--- src/pages/Register/styles.ts
+++ src/pages/Register/styles.ts
@@ -1,3 +1,108 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  max-width: 736px;
+  margin: 0 auto;
+  padding: 40px 20px;
+
+  h1 {
+    font-weight: 500;
+    font-size: 36px;
+    line-height: 54px;
+    color: #363f5f;
+    text-align: center;
+  }
+
+  form {
+    margin: 40px auto;
+    padding: 64px;
+    max-width: 730px;
+    background: #fff;
+    border-radius: 8px;
+
+    display: flex;
+    flex-direction: column;
+
+    fieldset {
+      min-inline-size: auto;
+      border: 0;
+
+      legend {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+
+        h2 {
+          font-size: 18px;
+          font-weight: 500;
+          color: #ed1236;
+        }
+      }
+    }
+  }
+`;
+
+export const Field = styled.div`
+  flex: 1;
+
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 24px;
+
+  label {
+    font-size: 14px;
+    margin-bottom: 8px;
+    color: #afafaf;
+  }
+
+  input {
+    flex: 1;
+    background: #f0f0f5;
+    border-radius: 8px;
+    border: 0;
+    padding: 16px 24px;
+    font-size: 16px;
+    color: #6c6c80;
+  }
+
+  & + div {
+    margin-left: 10px;
+  }
+
+  textarea {
+    flex: 1;
+    background: #f0f0f5;
+    border-radius: 8px;
+    border: 0;
+    padding: 16px 24px;
+    font-size: 16px;
+    color: #6c6c80;
+  }
+`;
+
+export const FieldGroup = styled.section`
+  display: flex;
+  flex: 1;
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  height: 56px;
+  background: #ed1236;
+  border-radius: 8px;
+  color: #fff;
+  font-weight: bold;
+  font-size: 16px;
+  border: 0;
+  align-self: flex-end;
+  margin-top: 40px;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background: ${darken(0.2, '#ed1236')};
+  }
+`;
